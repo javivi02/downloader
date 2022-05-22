6cfd217025bb7c5df20329ee7773519da4739b51
Javier <javivi02@gmail.com>
2022-05-22 17:51:22 +0200

    Version Final !!!

--- Menu.js
+++ Menu.js
@@ -29,7 +29,7 @@ export const inquirerMenu = async () => {
 
     console.clear();
     console.log('=================================='.green);
-    console.log('  Estherin Seleccione una opción'.white);
+    console.log('  Estherin selecciona una opción'.white);
     console.log('==================================\n'.green);
 
     const {opcion} = await inquirer.prompt(preguntas);
